dbd987a4ede21a4ff90f9385a8d3299303cdfaee
xbwang <vincent.wangxb@gmail.com>
2011-02-21 18:49:25 -0600

    hw6 problem2 updated,good

--- hw6/problem2.cpp
+++ hw6/problem2.cpp
@@ -14,14 +14,36 @@ using namespace std;
 //For example, two threads try to call bump at the same time and updated a==3 by a+=1.
 //However, the proper way is that one of the threads should wait for the other finishing updating
 //then both of them will get the right results.
+
+mutex some_mutex;
 class A
 {
 public:
     A() : a(0) {} 
-    void bump(int i = 1){
+    void bump(int i){
         std::lock_guard<std::mutex> guard(m);
-        a += i;
+		cout << "Updates a to be " << (a+=i) << "." << endl;
     }
     int a;
     mutex m;
 };
+
+A a;
+void test(int n)
+{
+	for(int i = 0; i < 20; i++){
+		a.bump(1);
+	}
+}
+
+int main()
+{
+	std::thread threadOne(test, 1);
+	std::thread threadTwo(test, 2);
+	std::thread threadThree(test, 3);
+	
+	threadOne.join();
+	threadTwo.join();
+	threadThree.join();
+	return 0;
+}
